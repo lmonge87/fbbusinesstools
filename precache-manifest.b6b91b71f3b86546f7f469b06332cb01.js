649d2ffab455edfe83a7f2d04823e58b98d47443
lmonge87 <lmonge87@gmail.com>
2020-09-08 16:55:45 -0600

    Updates

---
+++ precache-manifest.b6b91b71f3b86546f7f469b06332cb01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fe05b63adf9d6ed16e70e6ae098593",
+    "url": "/fbbusinesstools/index.html"
+  },
+  {
+    "revision": "26fde08766a68b32fbee",
+    "url": "/fbbusinesstools/static/css/2.9809d50e.chunk.css"
+  },
+  {
+    "revision": "cedbfb0a891aeeecfc46",
+    "url": "/fbbusinesstools/static/css/main.e818d4d6.chunk.css"
+  },
+  {
+    "revision": "26fde08766a68b32fbee",
+    "url": "/fbbusinesstools/static/js/2.727efc9f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fbbusinesstools/static/js/2.727efc9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cedbfb0a891aeeecfc46",
+    "url": "/fbbusinesstools/static/js/main.2255f9e0.chunk.js"
+  },
+  {
+    "revision": "a3e4a495f671c982f61b",
+    "url": "/fbbusinesstools/static/js/runtime-main.45117854.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FbAPIConnect/precache-manifest.e9e6f59ef17d13f68c1c9108796179fc.js"
+  "/fbbusinesstools/precache-manifest.b6b91b71f3b86546f7f469b06332cb01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FbAPIConnect/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fbbusinesstools/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
