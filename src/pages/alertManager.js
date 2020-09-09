f6b52415697d69087cb1ac285d62a78e44440b71
lmonge87 <lmonge87@gmail.com>
2020-09-08 18:39:08 -0600

    Account list check

--- src/pages/alertManager.js
+++ src/pages/alertManager.js
@@ -8,6 +8,8 @@ import Card from "react-bootstrap/Card";
 import { useLocalStorage } from "web-api-hooks";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { GrFacebook } from "react-icons/gr";
+import { RiCheckFill } from "react-icons/ri";
+import { CgClose } from "react-icons/cg";
 import { AlertOptions } from "../data/alertOptions.json";
 import { AdAccounts } from "../data/adAccountData.json";
 
@@ -87,6 +89,10 @@ export default function AlertManager() {
   const initialInputState = { School: "", Alert: "" };
   const [userInput, setUserInput] = useState(initialInputState);
   const [alertList, setAlertList] = useLocalStorage("alertList", []);
+  const [checkedAccounts, setCheckedAccounts] = useLocalStorage(
+    "checkedAccounts",
+    []
+  );
   const [adAccounts, setAdAccounts] = useState(accountsDeepCopy);
   const filteredAccounts = useMemo(
     () => filterAccounts(adAccounts, alertList),
@@ -94,13 +100,11 @@ export default function AlertManager() {
   );
 
   const date = new Date();
-  const formattedDate = date
-    .toLocaleDateString("en-US", {
-      day: "numeric",
-      month: "short",
-      year: "numeric",
-    });
-    
+  const formattedDate = date.toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -119,13 +123,19 @@ export default function AlertManager() {
   };
 
   const handleAlertDelete = (alert) => {
-    setAlertList(alertList.filter((filteredAlert) => filteredAlert !== alert));
+    setAlertList(alertList.filter((f) => f !== alert));
     setAdAccounts(accountsDeepCopy);
   };
 
   const handleClear = () => {
     setAlertList([]);
     setAdAccounts(accountsDeepCopy);
+  };
+
+  const handleCheck = (id) => {
+    checkedAccounts.includes(id)
+      ? setCheckedAccounts(checkedAccounts.filter((i) => i !== id))
+      : setCheckedAccounts([...checkedAccounts, id]);
   };
 
   const openAccountFB = (id) => {
@@ -145,139 +155,164 @@ export default function AlertManager() {
     <Card className="mainCardAlerts" text="white">
       <Card.Header>Alert Manager</Card.Header>
       <Card.Body>
-      <Row className="mt-3">
-        <Col xs="12">
-          <Form onSubmit={handleSubmit}>
-            <Form.Row className="align-items-center">
-              <Col md="4" xs="auto">
-                <Form.Label srOnly />
-                <Form.Control
-                  className="mb-2"
-                  as="select"
-                  placeholder="School Name"
-                  name="School Name"
-                  value={userInput.School}
-                  onChange={handleChangeSchool}
-                >
-                  <option hidden>Select School Name</option>
-                  {AlertOptions.Schools &&
-                    AlertOptions.Schools.map((i, index) => (
-                      <option key={index} value={i}>
-                        {i}
-                      </option>
+        <Row className="mt-3">
+          <Col xs="12">
+            <Form onSubmit={handleSubmit}>
+              <Form.Row className="align-items-center">
+                <Col md="4" xs="auto">
+                  <Form.Label srOnly />
+                  <Form.Control
+                    className="mb-2"
+                    as="select"
+                    placeholder="School Name"
+                    name="School Name"
+                    value={userInput.School}
+                    onChange={handleChangeSchool}
+                  >
+                    <option hidden>Select School Name</option>
+                    {AlertOptions.Schools &&
+                      AlertOptions.Schools.map((i, index) => (
+                        <option key={index} value={i}>
+                          {i}
+                        </option>
+                      ))}
+                  </Form.Control>
+                </Col>
+                <Col md="4" xs="auto">
+                  <Form.Control
+                    className="mb-2"
+                    as="select"
+                    placeholder="Alert Message"
+                    name="Alert Message"
+                    value={userInput.Alert}
+                    onChange={handleChangeAlert}
+                  >
+                    <option hidden>Select Alert Type</option>
+                    {AlertOptions.Alerts &&
+                      AlertOptions.Alerts.map((i, index) => (
+                        <option key={index} value={i}>
+                          {i}
+                        </option>
+                      ))}
+                  </Form.Control>
+                </Col>
+                <Col xs="auto">
+                  <Button
+                    disabled={!(userInput.School && userInput.Alert)}
+                    type="submit"
+                    className="mb-2"
+                  >
+                    Add
+                  </Button>
+                </Col>
+                <Col xs="auto">
+                  <Button
+                    onClick={handleClear}
+                    disabled={!alertList.length}
+                    className="mb-2"
+                    variant="danger"
+                  >
+                    Clear Alerts
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col>
+            <Card bg="dark" text="white">
+              <Card.Header>Alerts</Card.Header>
+              <Card.Body className="cardContainer">
+                <ListGroup>
+                  {alertList &&
+                    alertList.map((i, index) => (
+                      <ListGroup.Item
+                        className="d-flex"
+                        variant="dark"
+                        key={index}
+                      >
+                        <div className="flex-grow-1">
+                          <Card.Subtitle>{i.School}</Card.Subtitle>
+                          <Card.Text>
+                            {i.Alert} <br />
+                            <small className="text-muted">
+                              {formattedDate}
+                            </small>
+                          </Card.Text>
+                        </div>
+                        <Button
+                          variant="outline-danger"
+                          onClick={() => handleAlertDelete(i)}
+                          className="text-right, align-top"
+                        >
+                          <FaRegTrashAlt />
+                        </Button>
+                      </ListGroup.Item>
                     ))}
-                </Form.Control>
-              </Col>
-              <Col md="4" xs="auto">
-                <Form.Control
-                  className="mb-2"
-                  as="select"
-                  placeholder="Alert Message"
-                  name="Alert Message"
-                  value={userInput.Alert}
-                  onChange={handleChangeAlert}
-                >
-                  <option hidden>Select Alert Type</option>
-                  {AlertOptions.Alerts &&
-                    AlertOptions.Alerts.map((i, index) => (
-                      <option key={index} value={i}>
-                        {i}
-                      </option>
-                    ))}
-                </Form.Control>
-              </Col>
-              <Col xs="auto">
-                <Button
-                  disabled={!(userInput.School && userInput.Alert)}
-                  type="submit"
-                  className="mb-2"
-                >
-                  Add
-                </Button>
-              </Col>
-              <Col xs="auto">
-                <Button
-                  onClick={handleClear}
-                  disabled={!alertList.length}
-                  className="mb-2"
-                  variant="danger"
-                >
-                  Clear Alerts
-                </Button>
-              </Col>
-            </Form.Row>
-          </Form>
-        </Col>
-      </Row>
-      <hr />
-      <Row>
-        <Col>
-          <Card bg="dark" text="white">
-            <Card.Header>Alerts</Card.Header>
-            <Card.Body className="cardContainer">
-              <ListGroup>
-                {alertList &&
-                  alertList.map((i, index) => (
-                    <ListGroup.Item
-                      className="d-flex"
-                      variant="dark"
-                      key={index}
+                </ListGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col>
+            <Card bg="dark" text="white">
+              <Card.Header>Affected Accounts</Card.Header>
+              <Card.Body className="cardContainer">
+                {filteredAccounts &&
+                  filteredAccounts.map((i, index) => (
+                    <Card
+                      bg={
+                        checkedAccounts.includes(i.id) ? "success" : "secondary"
+                      }
+                      text="white"
+                      key={i.id}
                     >
-                      <div className="flex-grow-1">
-                        <Card.Subtitle>{i.School}</Card.Subtitle>
-                        <Card.Text>
-                          {i.Alert} <br/>
-                          <small className="text-muted">{formattedDate}</small>
-                        </Card.Text>
-                      </div>
-                      <Button
-                        variant="outline-danger"
-                        onClick={() => handleAlertDelete(i)}
-                        className="text-right, align-top"
-                      >
-                        <FaRegTrashAlt />
-                      </Button>
-                    </ListGroup.Item>
+                      <Card.Header className="d-flex align-items-center">
+                        <div className="flex-grow-1">
+                          <Card.Subtitle>
+                            {i.name}, should be turned off.
+                          </Card.Subtitle>
+                          <Card.Text>
+                            {i.backOnConditions.goesBackOn
+                              ? `Those campaigns should be turned back on at: ${i.backOnConditions.time}`
+                              : "Those campaigns should not go back on"}
+                          </Card.Text>
+                        </div>
+                        <Button
+                          variant="primary"
+                          onClick={() => openAccountFB(i.id)}
+                          className="text-right, align-top"
+                        >
+                          <GrFacebook />
+                        </Button>
+                        {checkedAccounts.includes(i.id) ? (
+                          <Button
+                            variant="outline-danger"
+                            className="ml-2"
+                            onClick={() => handleCheck(i.id)}
+                          >
+                            <CgClose />
+                          </Button>
+                        ) : (
+                          <Button
+                            variant="outline-success"
+                            className="ml-2"
+                            onClick={() => handleCheck(i.id)}
+                          >
+                            <RiCheckFill />
+                          </Button>
+                        )}
+                      </Card.Header>
+                    </Card>
                   ))}
-              </ListGroup>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <hr />
-      <Row>
-        <Col>
-          <Card bg="dark" text="white">
-            <Card.Header>Affected Accounts</Card.Header>
-            <Card.Body className="cardContainer">
-              {filteredAccounts &&
-                filteredAccounts.map((i, index) => (
-                  <Card bg="secondary" text="white" key={index}>
-                    <Card.Header className="d-flex align-items-center">
-                      <div className="flex-grow-1">
-                        <Card.Subtitle>
-                          {i.name}, should be turned off.
-                        </Card.Subtitle>
-                        <Card.Text>
-                          {i.backOnConditions.goesBackOn
-                            ? `Those campaigns should be turned back on at: ${i.backOnConditions.time}`
-                            : "Those campaigns should not go back on"}
-                        </Card.Text>
-                      </div>
-                      <Button
-                        variant="primary"
-                        onClick={() => openAccountFB(i.id)}
-                        className="text-right, align-top"
-                      >
-                        <GrFacebook />
-                      </Button>
-                    </Card.Header>
-                  </Card>
-                ))}
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Card.Body>
     </Card>
   );
